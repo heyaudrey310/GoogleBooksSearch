affbd4aebc29ad13d605ce5ea4c1db793717386f
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-07-04 11:56:00 -0400

    add grid folder

---
+++ google-books-app/client/src/components/form/input.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Label } from "./label";
+
+export const Input = props => (
+  <div className="form-group">
+    <Label htmlFor={props.name}>{props.label}</Label>
+    <input className="form-control" {...props}/>
+  </div>
+);
